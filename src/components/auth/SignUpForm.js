2ac023ae5393a007fdcdab9b96a25b71c8cf6a84
shazilhamzah <140880172+shazilhamzah@users.noreply.github.com>
2024-10-04 12:44:38 +0500

    Update SignUpForm.js

--- src/components/auth/SignUpForm.js
+++ src/components/auth/SignUpForm.js
@@ -140,7 +140,7 @@ export function SignUpForm() {
             >
               <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
               <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                Google
+                Continue with Google
               </span>
               <BottomGradient />
             </button>
